4945de4aab23718467319636c0dd06da05f9a9e0
Marwa Nasser <marwa.nasser8133@gmail.com>
2025-04-15 05:35:08 +0200

    Add Footer component and updated navbar

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { createPinia } from 'pinia'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import NavBar from './components/NavBar.vue'
+import Footer from './components/Footer.vue'
 
 import 'bootstrap'
 const app = createApp(App)
@@ -12,4 +13,6 @@ app.use(createPinia())
 app.use(router)
 
 app.component('NavBar', NavBar)
+app.component('Footer', Footer)
+
 app.mount('#app')
